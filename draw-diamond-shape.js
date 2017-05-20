ec365b8bfa1d674c72950df7529d3ffff1887b73
Hien Luong <phuhien@uw.edu>
2017-05-20 16:48:55 -0700

    Add files via upload

---
+++ draw-diamond-shape.js
@@ -0,0 +1,33 @@
+/*
+
+   *   
+  ***  
+ ***** 
+*******
+ ***** 
+  ***  
+   *   
+
+*/
+
+function drawDiamond(n) {
+    var reduce = 2;
+    var star = 1, space = (n - 1)/2;
+    
+    for (var i = 0; i < n; i++) {
+    console.log(loopStr(space," ") + loopStr(star,"*") + loopStr(space," "));
+    star  = star + reduce;
+    space = ((n - star)/2);
+    if (star == n) reduce = -2;
+    }
+}
+
+function loopStr(n,str) {
+    var result = "";
+    for (var i = 0; i < n; i++) {
+        result += str;
+    }
+    return result;
+}
+
+console.log(drawDiamond(7))
---
+++ pine-tree-draw.js
@@ -0,0 +1,63 @@
+/*
+
+        *        
+       ***       
+      *****      
+       ***       
+      *****      
+     *******     
+      *****      
+     *******     
+    *********    
+     *******     
+    *********    
+   ***********   
+    *********    
+   ***********   
+  *************  
+   ***********   
+  *************  
+ *************** 
+  *************  
+ *************** 
+*****************
+        *        
+        *        
+ *************** 
+
+*/
+function loopStr(n,str) {
+    var result = "";
+    for (var i = 0; i < n; i++) {
+        result += str;
+    }
+    return result;
+}
+    
+function root(max,reduce) {
+    star = max - reduce;
+    space= (max - star) / 2;
+    console.log(loopStr(space," ") + loopStr(star,"*") + loopStr(space," "));
+}
+
+// pine tree 
+function drawPineTree(n,m) {
+    var max  = 2*(m+1) + 1;
+    for (var row = 0; row < m; row++) {
+        
+        var star = 1 + row*2;
+        var space= (max - star)/2; 
+        
+        for (var i = 0; i < n; i++) {
+          console.log(loopStr(space," ") + loopStr(star,"*") + loopStr(space," "));
+          star  = star + 2;
+          space = space - 1;
+        }
+    }
+    
+    root(max,max-1);
+    root(max,max -1);
+    root(max,2);
+}
+
+console.log(drawPineTree(3,4))
